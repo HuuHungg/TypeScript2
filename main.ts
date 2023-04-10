2dff92548c4249a7c87e7e9888b26853c9cc279b
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-10 22:23:14 +0700

    add file modifile

---
+++ main.ts
@@ -0,0 +1,29 @@
+// lesson21
+let age: number = 25
+
+
+switch(age) {
+    case 25:
+        console.log("I am writing email send to Face")
+        break;
+    case 18:
+        console.log("毎日ITを勉強してる頑張りましょう")
+        break;
+    default:
+        console.log("Run default")   
+}
+
+
+// lesson21
+let fullName: string = "Huu Hung Nguyen"
+
+switch(fullName) {
+    case "Huu Hung":
+        console.log("毎日二時間ぐらいITを勉強してる頑張りましょう")
+        break
+    case "Huu Hung Nguyen":
+        console.log("I am studying IT")
+        break
+    default:
+        console.log("頑張ってきださいね")
+}
