993edf0d1918dec478b26ab208ac975348c9ab7e
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-11 21:05:31 +0700

    add file modifile

--- main.ts
+++ main.ts
@@ -1,63 +1,23 @@
-let products = [
-    {name : 'phone', price: 700},
-    {name: "tablet", price: 900},
-    {name: "laptop", price: 1200},
-    {name: "macbook", price: 1600}
-];
-
-// for (let i = 0; i < products.length; i++) {
-//     if( products[i].price === 1200) {
-//         break;
-//     }
-//     console.log(products[i])
-// }
-
-let discount1 = 0;
-let product = products[2]
-switch (product.name) {
-    case 'phone':
-        discount1 = 5;
-        break;
-    case 'tablet':
-        discount1 = 10
-        break
-    case 'laptop':
-        discount1 = 20
-        // break;
-    default:    
-        discount1 = 100
-}
-
-console.log(`There is a ${discount1}% on ${product.name}.`)
-
-let color = "red"
-switch(color) {
-    case "blue":
-        console.log("The color is blue")
-        break;
-    case "red":
-        console.log("The color is red")
-        break;
-    case "black":
-        console.log("The color is black")
-        break;
-    default :
-        console.log("color is not define")
-}
-
-let i = 0
-while (i < 10) {
-    console.log(i)
-    i++
-    if(i === 5) {
-        break;
-    }
-}
-
-const arr = [1, 2, 3, 4, 5];
+const arr = [1,2,3,4,5,6,7,8,9,10]
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === 3) {
-    console.log("Found 3 at index", i);
-    break;
-  }
+    if(arr[i] % 2 !== 0) {
+        continue
+    }
+    console.log(arr[i])
+}
+
+const arr2 = [16, null, 22, undefined, 33, "string"];
+for(let i = 0; i < arr2.length; i++) {
+    if(arr2[i] === null || arr2[i] === undefined) {
+        continue
+    }
+    console.log(arr2[i])
+}
+
+const arr3 = [11, "two", 333, "for", 33333444];
+for(let i = 0; i < arr3.length; i++) {
+    if(typeof arr3[i] !== "number") {
+        continue
+    }
+    console.log(arr3[i])
 }
