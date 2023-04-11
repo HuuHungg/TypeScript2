ab035c346466df04ec3cbad942b4235fe1553637
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-11 15:14:14 +0700

    add file moddifle

--- main.ts
+++ main.ts
@@ -1,29 +1,42 @@
-// lesson21
-let age: number = 25
+// TH1
+for(let i = 0; i < 10; i++) {
+    console.log(">>> i=", i)
+} 
 
+console.log("=====")
+// TH2
+let n = 12;
+let sum = 0;
+for(let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log(sum)
 
-switch(age) {
-    case 25:
-        console.log("I am writing email send to Face")
-        break;
-    case 18:
-        console.log("毎日ITを勉強してる頑張りましょう")
-        break;
-    default:
-        console.log("Run default")   
+console.log("=====")
+// TH3
+let arr = [1,2,3,4,5,6]
+for(let a = 0 ; a < arr.length; a++) {
+    console.log(arr[a])
 }
 
+console.log("=====")
+// TH4
 
-// lesson21
-let fullName: string = "Huu Hung Nguyen"
-
-switch(fullName) {
-    case "Huu Hung":
-        console.log("毎日二時間ぐらいITを勉強してる頑張りましょう")
-        break
-    case "Huu Hung Nguyen":
-        console.log("I am studying IT")
-        break
-    default:
-        console.log("頑張ってきださいね")
+let arr2 = [1,2,3,4,5,6,7,7,8]
+let sum2 = 0
+for(let b = 0; b < arr2.length; b++) {
+    sum2 += arr2[b]
 }
+
+console.log(sum2)
+
+console.log("=====")
+// TH5
+let person = { name: 'John', age: 30, gender: 'male' };
+let fullPerson = ""
+for (let key in person) {
+  fullPerson += `${key}: ${person[key]}`;
+}
+console.log(fullPerson)
+
+
