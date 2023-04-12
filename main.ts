3c847310c0f3696dd5dfc0b897aed3a856ab3b3a
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-12 14:57:11 +0700

    add file modifile

--- main.ts
+++ main.ts
@@ -10,3 +10,12 @@ let sum2 = (a:number, b: number) =>  {
     return a + b    
 }
 console.log(">>> check sum: ", sum2(2,3) )
+
+
+let sum3 = (x: number, y: number) => {
+    return "毎日ITを勉強してる頑張りましょう"
+}
+
+console.log('check sum', sum3(1,20))
+
+
