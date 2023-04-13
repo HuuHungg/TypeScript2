223a2f73de02d09f528533ac92bb9109bdd02b6b
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-13 18:13:00 +0700

    add file modifile

--- main.ts
+++ main.ts
@@ -1,19 +1,17 @@
-// let sum7 = (x: number, y: number, z = false) => {
-//     if( z === false) {
-//         return x + y
-//     }
-//     if(z) {
-//         return x - y
-//     }
-// }
+function sum(a: number, b: number): number;
+function sum(a: string, b: string): string;
+function sum(a: any,  b: any): any {
+    return a + b
+}
 
-// console.log(">>> check sum 7 = ", sum7(1,2), sum7(1, 2, true))
+console.log(sum(50,60))
+console.log(sum("Huu Hung", "Nguyen"))
+console.log(sum( "Name", " I am leaning English "))
 
-function greet (name?:string, greeting: string = "Hello") {
-    console.log( `${greeting}, ${name ?? 'world'}!`);
-}    
-    greet()
-    greet('John')
-    greet(undefined, 'Hola')
-    greet('Jane', 'Hi')
+function addNew(a: number, b: number): number;
+function addNew(a: string, b: string): string;
+function addNew(a: any, b: any) {
+    return a + b
+}
 
+console.log(addNew(10,20), addNew("毎日ITを勉強してる", "頑張ってください"))
