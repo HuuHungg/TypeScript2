aba266a000c2cff14b3aeea17b5c1eebb0b8ae38
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-13 22:30:17 +0700

    add file modifile

--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
-function sum(a: number, b: number): number;
-function sum(a: string, b: string): string;
-function sum(a: any,  b: any): any {
-    return a + b
-}
+class Person {
+    ssn : string;
+    firstName: string;
+    lastName: string;
 
-console.log(sum(50,60))
-console.log(sum("Huu Hung", "Nguyen"))
-console.log(sum( "Name", " I am leaning English "))
+    constructor(ssn: string , firstName: string, lastName: string ) {
+        this.ssn = ssn
+        this.firstName = firstName
+        this.lastName = lastName
+    }
 
-function addNew(a: number, b: number): number;
-function addNew(a: string, b: string): string;
-function addNew(a: any, b: any) {
-    return a + b
-}
+    getFullName(): string {
+        return `calling method: ${this.firstName} ${this.lastName}` ;
+    }
+}   
 
-console.log(addNew(10,20), addNew("毎日ITを勉強してる", "頑張ってください"))
+let fullCourse = new Person("毎日","日本語を勉強してる", "頑張ってください")
+
+console.log(fullCourse)
+console.log(fullCourse.getFullName())
+
+
