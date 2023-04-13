671364328e46d7c708c576f4fa909a903812995d
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-13 11:08:04 +0700

    add file modifile

--- main.ts
+++ main.ts
@@ -1,26 +1,19 @@
-// Optional có thể có hoặc không 
+// let sum7 = (x: number, y: number, z = false) => {
+//     if( z === false) {
+//         return x + y
+//     }
+//     if(z) {
+//         return x - y
+//     }
+// }
 
-let sum5 = ( x: number, y: number, z ?: number) => {
-    if(z) return x + y + z
-    return x + y
-}
+// console.log(">>> check sum 7 = ", sum7(1,2), sum7(1, 2, true))
 
-console.log(">>> check sum 5 = ", sum5(2,5,10))
+function greet (name?:string, greeting: string = "Hello") {
+    console.log( `${greeting}, ${name ?? 'world'}!`);
+}    
+    greet()
+    greet('John')
+    greet(undefined, 'Hola')
+    greet('Jane', 'Hi')
 
-
-interface Person {
-    name: string;
-    age ?: number;
-}
-
-function greet (person: Person) {
-    console.log(`Hello, ${person.name}!`)
-    if(person.age) {
-        console.log(`You are ${person.age} years old` )
-    }else {
-        console.log(`I don't know year old`)
-    }
-}
-
-greet({name: "john"})
-greet({name: "Jane", age: 30})
