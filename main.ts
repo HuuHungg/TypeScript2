b327759b9e073c5836aabdc3773ee0d5deb42334
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-17 17:43:01 +0700

    add file modifile

--- main.ts
+++ main.ts
@@ -1,39 +1,60 @@
-abstract class Employee99 {
-    constructor(private firstName: string, private lastName: string) {}
-    abstract getSalary(): number; // abstract method
-    
-    // normal method
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`
-    }
-    
-    compensationStatement(): string {
-        return ` ${this.fullName} makes ${this.getSalary()} a month`
-    }
+interface IPerson {
+    firstName: string,
+    lastName : string
 }
 
-class FullTimeEmployee extends Employee99 {
-    constructor(firstName: string, lastName: string, private salary: number) {
-        super(firstName, lastName); // 
-    }
-
-    getSalary(): number {
-        return this.salary;
-    }
- }
-
-class Contractor extends Employee99 {
-    constructor (firstName: string, lastName: string, private rate: number) {
-        super(firstName, lastName)
-    }
-    getSalary(): number {
-        return this.rate;
-    }
+function getFullName(person: IPerson) {
+    console.log(`${person.firstName} ${person.lastName}`) 
 }
 
-const test1 = new FullTimeEmployee("Yushing","Fufun", 4000)
-console.log(">>> test 1: ", test1.getSalary())
+
+let myObj = {
+    firstName: "天気はどうですか",
+    lastName: "今日はいい天気です"
+}
+
+getFullName(myObj)
 
 
+interface User {
+    username: string;
+    email: string;
+    password: string;
+    age ?: number;
+}
 
+function registerUser(user: User) {
+    console.log(`This is ${user.username} ${user.email} ${user.password} ${user.age}`)
+}
 
+const newUser: User = {
+    username: "Yushing",
+    email: "yushing02@gmail.com",
+    password: "I don't know",
+    age : 21
+}
+
+registerUser(newUser)
+
+interface Product {
+    name: string,
+    price: number,
+    description ?: string,
+    image ?: string[],
+}
+
+const laptop:Product = {
+    name: "Macbook Pro",
+    price: 1980,
+    description: "The ulitimate laptop for professionals",
+    image: ["Macbook-pro1", "Macbook-pro2"]
+}
+
+console.log(laptop)
+
+const mouse: Product = {
+    name: "Logitech Mx master",
+    price: 99.99
+}
+
+console.log(mouse)
